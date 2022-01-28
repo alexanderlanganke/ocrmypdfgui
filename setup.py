737f9a714a7ac489172f832db3b182eab4ab9644
alexanderlanganke <alexlanganke@gmail.com>
2022-01-28 14:39:12 +0100

    work on snap core18

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.8",
+    python_requires=">=3.6",
     install_requires=[
         'cffi >= 1.9.1',  # must be a setup and install requirement
         'coloredlogs >= 14.0',  # strictly optional
